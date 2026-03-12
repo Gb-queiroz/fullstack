4cb2c8e117b8ebcc2bc9092a1e30c16412724b6a
Gb-queiroz <gabriequeirozs2006@gmail.com>
2026-03-12 20:46:28 -0300

    Add files via upload

---
+++ teste/arquivo.js
@@ -0,0 +1,19 @@
+window.alert("ESSE É UM ALERTA");
+console.log("isso aqui é um arquivo");
+
+let dado = prompt("insira um dado");
+
+console.log(dado);
+
+let i = 0
+
+while(i<10){
+    window.alert(i);
+    i++;
+}
+
+for(let i=0; i < 10; i++){
+    window.alert(i);
+}
+
+Math.random() * (1- 0) 
